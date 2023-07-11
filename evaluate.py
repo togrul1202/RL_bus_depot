981ed27b1c2e36dbfe41dc8e6d928a08b83c8739
Toghrul <togrul1202@gmail.com>
2023-07-11 13:50:15 +0200

    reqs update

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,4 @@
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
 from statistics import mean
 
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
 from statistics import mean
 
